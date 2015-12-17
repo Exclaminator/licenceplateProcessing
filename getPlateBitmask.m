a2089d11f225bc346e1dc97ae002e15d378c439f
Naomi de Ridder <naomi-ah@hotmail.com>
2015-12-17 11:13:29 +0100

    Changing the mask

--- getPlateBitmask.m
+++ getPlateBitmask.m
@@ -1,8 +1,12 @@
+%Het filter dat we gaan gebruiken om de gele nummerplaat beter te kunnen
+%herkennen. Rood op kanaal 1, blauw op kanaal 2 en groen op kanaal 3 met
+%het masker zoals we deze met colorThresholder hebben uitgerekend. (Moet
+%nog worden aangepast!)
 function [ mask ] = getPlateBitmask( img )
     %img = ind2rgb(image);
     imgRed = img(:,:,1);
     imgBlue = img(:,:,2);
     imgGreen = img(:,:,3);
-    mask = imgRed > 160 & imgRed < 240 & imgBlue > 125 & imgBlue < 175 & imgGreen > 65 & imgGreen < 110;
+    mask = imgRed > 160 & imgRed < 230 & imgBlue > 60 & imgBlue < 130 & imgGreen > 125 & imgGreen < 205;
 end
 
