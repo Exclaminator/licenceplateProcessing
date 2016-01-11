1746e97168793c8d8484ff4a547e847c74abfe0f
Naomi <naomi-ah@hotmail.com>
2016-01-11 10:50:00 +0100

    Buitenrand is weg

--- uitvoerbareCode.m
+++ uitvoerbareCode.m
@@ -12,5 +12,17 @@ plate=cutInCorners(deskewIm, cornersNew);
 
 binIm= im2bw(plate, 0.5);
 IM2= imcomplement(binIm);
-dip_image(IM2) %linear stretch toepassen
-label=bwlabel(IM2);
+dip_image(IM2) 
+
+connectivity=bwconncomp(IM2);
+numPixels=cellfun(@numel,connectivity.PixelIdxList);
+
+%Je haalt het eerste object weg als deze (bijvoorbeeld) groter is dan 150.
+%Dit moet een getal zijn dat altijd groter is dan de letters. Het verschil
+%zou groot genoeg moeten zijn om ook een kleine buitenrand weg te halen.  
+if numPixels(1) > 150
+    IM2(connectivity.PixelIdxList{1})=0;
+end
+
+label=bwlabel(IM2); %bwlabeln(IM2) kan ook hier. 
+dip_image(label) %linear stretch toepassen
