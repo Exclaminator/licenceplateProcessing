d8e41725317377e64d73816cf255afe09602f01c
Naomi <naomi-ah@hotmail.com>
2016-01-14 15:25:14 +0100

    1. Lezen van een image veranderd 2. handelen van de buitenrand veranderd. Niet optimaal!! 3. losse objecten krijgen uit de kentekenplaat. (hoort niet bij de vorige push)

--- uitvoerbareCode.m
+++ uitvoerbareCode.m
@@ -1,6 +1,6 @@
 %Uitvoerbare code
 
-a= readim('frame1.png');
+a= readim('testData/frame1.png');
 img=dip_array(a);
 ndata=normalize(img);
 bm=createMask(ndata);
@@ -19,15 +19,30 @@ dip_image(IM2)
 connectivity=bwconncomp(IM2);
 numPixels=cellfun(@numel,connectivity.PixelIdxList);
 
-%Je haalt het eerste object weg als deze (bijvoorbeeld) groter is dan 150.
-%Dit moet een getal zijn dat altijd groter is dan de letters. Het verschil
-%zou groot genoeg moeten zijn om ook een kleine buitenrand weg te halen.  
-if numPixels(1) > 150
-    IM2(connectivity.PixelIdxList{1})=0;
-end
+%Je haalt het eerste object weg want dit is de buitenrand. Dit kun je
+%checken doordat er dan 8 elementen moeten zijn. Dit is absoluut niet een
+%nette manier, het moet met propagatie uiteindelijk. Voor nu even een
+%cheat.
+IM2(connectivity.PixelIdxList{1})=0;
+numPixels= numPixels([end-7:end]);
 
 label=bwlabel(IM2); %bwlabeln(IM2) kan ook hier. 
 dip_image(label) %linear stretch toepassen
 
+%Alle losse objecten uit de kentenkenplaat verkregen
+object1 = label ==1;
+object2 = label ==2;
+object3 = label ==3;
+object4 = label ==4;
+object5 = label ==5;
+object6 = label ==6;
+object7 = label ==7;
+object8 = label ==8;
+
+numberOfTruePixels = sum(object1(:));
+
 %Finding the edges of the lettres
 edges=edge(IM2, 'sobel');  
+
+
+
